7b123f68d89db48ff85a2c60c79494f4b136f1f5
Dino Krso <dino.krso@stu.ibu.edu.ba>
2025-06-25 01:09:14 +0200

    ValjdaRadi

--- next.config.mjs
+++ next.config.mjs
@@ -7,15 +7,18 @@ const nextConfig = {
     ignoreDuringBuilds: true,
   },
   typescript: {
-    ignoreBuildErrors: true, // Or remove this typescript block entirely
+    ignoreBuildErrors: false,
   },
   images: {
     domains: ['i.ibb.co', 'via.placeholder.com'],
     unoptimized: true,
   },
+  swcMinify: true,
+  assetPrefix: process.env.NODE_ENV === 'production' ? '' : '',
   env: {
     MONGODB_URI: process.env.MONGODB_URI,
     NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
+    NEXTAUTH_URL: process.env.NEXTAUTH_URL,
     HCAPTCHA_SECRET_KEY: process.env.HCAPTCHA_SECRET_KEY,
   },
 }
