771e7e27105991fe20a63882bec161770e5d6228
Dino Krso <dino.krso@stu.ibu.edu.ba>
2025-06-25 00:59:14 +0200

    RenderBzs

--- next.config.mjs
+++ next.config.mjs
@@ -16,7 +16,6 @@ const nextConfig = {
   env: {
     MONGODB_URI: process.env.MONGODB_URI,
     NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
-    NEXTAUTH_URL: process.env.NEXTAUTH_URL,
     HCAPTCHA_SECRET_KEY: process.env.HCAPTCHA_SECRET_KEY,
   },
 }
