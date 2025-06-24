f805fcc335968a7804bcfc7e8d6561890a3e0af3
Dino Krso <dino.krso@stu.ibu.edu.ba>
2025-06-24 20:07:53 +0200

    Deploy Fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  experimental: {
+    serverComponentsExternalPackages: ['mongoose'],
+  },
   eslint: {
     ignoreDuringBuilds: true,
   },
@@ -7,7 +10,14 @@ const nextConfig = {
     ignoreBuildErrors: true,
   },
   images: {
+    domains: ['i.ibb.co', 'via.placeholder.com'],
     unoptimized: true,
+  },
+  env: {
+    MONGODB_URI: process.env.MONGODB_URI,
+    NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
+    NEXTAUTH_URL: process.env.NEXTAUTH_URL,
+    HCAPTCHA_SECRET_KEY: process.env.HCAPTCHA_SECRET_KEY,
   },
 }
 
